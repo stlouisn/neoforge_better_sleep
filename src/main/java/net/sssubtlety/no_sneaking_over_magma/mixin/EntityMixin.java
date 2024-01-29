2d215b01da0364f27bc0a79b82a6e78a794d4fbd
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2024-01-29 15:32:40 -0800

    updated deps for 1.20.4, replced mixin jank cast with shadow

--- src/main/java/net/sssubtlety/no_sneaking_over_magma/mixin/EntityMixin.java
+++ src/main/java/net/sssubtlety/no_sneaking_over_magma/mixin/EntityMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.MagmaBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,11 +18,13 @@ public abstract class EntityMixin {
 
     @Shadow public abstract BlockPos getLandingPosition();
 
+    @Shadow public abstract World getWorld();
+
     @ModifyExpressionValue(method = "bypassesSteppingEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isSneaking()Z"))
     private boolean tryNotCountingAsSneakingForMagma(boolean sneaking) {
         return sneaking && (
             shouldSneakingProtectFromMagma() ||
-            Blocks.MAGMA_BLOCK != ((Entity)(Object)this).getWorld().getBlockState(this.getLandingPosition()).getBlock()
+            Blocks.MAGMA_BLOCK != getWorld().getBlockState(this.getLandingPosition()).getBlock()
         );
     }
 }
